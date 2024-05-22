809acc870e033f139b7d1607934f16d04544ed70
montie <montiepy@gmail.com>
2024-05-21 21:48:14 -0600

    initcommit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        './Pages/**/*.cshtml', // Include all .cshtml files
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'pantpink': "#F8A3BC"
+            },
+        }
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
