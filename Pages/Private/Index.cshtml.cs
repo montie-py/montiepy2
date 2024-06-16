1eca46ca72b4de26848745f21e4523eeef832135
montie <montiepy@gmail.com>
2024-06-16 14:05:57 -0600

    setting up the admin layout

--- Pages/Private/Index.cshtml.cs
+++ Pages/Private/Index.cshtml.cs
@@ -6,6 +6,9 @@ namespace montiepy2.Pages.Private
     [Authorize]
     public class IndexModel : PageModel
     {
-
+        public void OnGet()
+        {
+            ViewData["area"] = "admin";
+        }
     }
 }
