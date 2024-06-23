eefdcf060f5dc33cd6d73292dbdd43e8e0b0bfb7
montie <montiepy@gmail.com>
2024-06-22 19:27:38 -0600

    adding new blog item functionality

--- Pages/Private/AddBlogEntry.cshtml.cs
+++ Pages/Private/AddBlogEntry.cshtml.cs
@@ -1,19 +1,33 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using montiepy.Services.Blog;
+using montiepy2.Models;
 
 namespace montiepy2.Pages.Private
 {
     [Authorize]
     public class AddBlogEntry : PageModel
     {
+        [BindProperty]
+        public required string Title { get; set; }
+        [BindProperty]
+        public required string Text { get; set; }
+
         public void OnGet()
         {
             ViewData["area"] = "admin";
         }
 
-        public void OnPost()
+        [HttpPost]
+        public IActionResult OnPost()
         {
-            
+            BlogEntry blogEntry = new();
+            blogEntry.Title = Title;
+            blogEntry.Text = Text;
+            blogEntry.Date = DateTime.Now;
+            BlogService.AddNewBlogEntry(blogEntry);
+            return RedirectToPage("/Private/Blog");
         }
     }
 }
--- Services/Blog/BlogService.cs
+++ Services/Blog/BlogService.cs
@@ -1,24 +1,45 @@
+using System.Text;
 using montiepy2.Models;
 
 namespace montiepy.Services.Blog
 {
     public static class BlogService
     {
+        private const string FileName = "blog.txt";
+        private const string EntryDelimiter = "---";
+        private const string EntryItemDelimiter = "|";
         public static List<BlogEntry> GetAllBlogEntries()
         {
             List<BlogEntry> BlogEntries = new();
-            string blogEntriesText = System.IO.File.ReadAllText("blog.txt");
-            string[] blogEntries = blogEntriesText.Split("---");
+            string blogEntriesText = File.ReadAllText(FileName);
+            string[] blogEntries = blogEntriesText.Split(EntryDelimiter);
             foreach (string blogEntry in blogEntries)
             {
+                if (String.IsNullOrEmpty(blogEntry)) {
+                    continue;
+                }
                 BlogEntry BlogEntry = new BlogEntry();
-                var entryParts = blogEntry.Split("|");
+                var entryParts = blogEntry.Split(EntryItemDelimiter);
                 BlogEntry.Title = entryParts[0];
                 BlogEntry.Date = DateTimeOffset.FromUnixTimeSeconds(long.Parse(entryParts[1])).DateTime;
                 BlogEntry.Text = entryParts[2];
                 BlogEntries.Add(BlogEntry);
             }
             return BlogEntries;
+        }
+
+        public static void AddNewBlogEntry(BlogEntry blogEntry)
+        {
+            var blogEntryRow = new StringBuilder();
+            blogEntryRow.AppendLine();
+            blogEntryRow.Append(blogEntry.Title);
+            blogEntryRow.Append(EntryItemDelimiter);
+            blogEntryRow.Append(((DateTimeOffset)blogEntry.Date).ToUnixTimeSeconds());
+            blogEntryRow.Append(EntryItemDelimiter);
+            blogEntryRow.Append(blogEntry.Text);
+            blogEntryRow.AppendLine();
+            blogEntryRow.Append(EntryDelimiter);
+            File.AppendAllText(FileName, blogEntryRow.ToString());
         }
     }
 }
