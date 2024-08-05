327b6ac6085bd178410b29ac3672acd3d0d5cd87
montie <montiepy@gmail.com>
2024-08-04 22:15:21 -0600

    Fixing VS code's generation of default files

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using montiepy2.Business.Services.Blog;
+using montiepy2.Business.Services.Review;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<IBlogService, BlogService>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
+
 
 var app = builder.Build();
 
@@ -9,17 +20,13 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
-app.UseRouting();
-
+// app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapRazorPages();
-
+app.MapDefaultControllerRoute();
 app.Run();
